28122f97c2c2104ff8b9e3481e38dd22d6c0393a
Larissa Thelin <larissathelin@Larissas-MacBook-Pro.local>
2021-03-31 13:41:14 -0600

    trying to figure out dist_water

--- scripts/08a_Debugging.R
+++ scripts/08a_Debugging.R
@@ -18,7 +18,7 @@ library(rgdal) # distance from points
 library(geosphere) # distance to water
 library(viridis) # for plotting (section 10)
 library(FNN) # for get.fnnx() - this doesn't work
-library(RANN) # for nn2() 
+library(RANN) # for nn2() in section 6
 
 theme_nuwcru <- function(){
   theme_bw() +
@@ -107,7 +107,7 @@ head(water_coordinates)
 str(water_coordinates)
 
 
-# 3. Loop with problem subset ---------------
+# 3. Loop #1 with problem subset (using: dist2Line, gDistance, get.knnx, or spDists) ---------------
 
 
 used_avail=subset(used_avail, select=-c(DIST_WATER))
@@ -129,10 +129,10 @@ str(used_avail_subset_spatial)
 # Use mdist (not mdist2), I just wanted to keep it in here so that I remember that I tried it!
 # Also, if you try mdist2, you need to use the polar projection, rather than lat/long
 
-#mdist <- list() # dist2Line: original option
-#mdist2 <- list() # gDistance: new option that doesn't work
-#mdist3 <- list()
-mdist4 <- list()
+#mdist <- list() # dist2Line: original option 
+#mdist2 <- list() # gDistance: doesn't work (water pixels aren't identified)
+#mdist3 <- list() # get.knnx: doesn't work (same as above)
+mdist4 <- list() # spDists: doesn't work (same as aboe)
 
 for(i in 1:nrow(used_avail_subset)){
   matching_raster <- water_coordinates[which(names(water_coordinates) == used_avail_subset[i,"date_char"])]
@@ -149,8 +149,8 @@ for(i in 1:nrow(used_avail_subset)){
     #proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
     proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # use for gDistance
   #mdist2[[i]] <- as.data.frame(apply(gDistance(xy_bear, xy_water, byid=TRUE),2,min))
-  #mdist3[[i]] <- as.data.frame(get.knnx(coordinates(xy_water), coordinates(xy_bear), k=1))
-  mdist4[[i]] <- as.data.frame(spDists(xy_water, xy_bear), longlat=TRUE)
+  mdist3[[i]] <- as.data.frame(get.knnx(coordinates(xy_water), coordinates(xy_bear), k=1))
+  #mdist4[[i]] <- as.data.frame(spDists(xy_water, xy_bear), longlat=TRUE)
   #mdist[[i]] <- data.frame(DIST_WATER=geosphere::dist2Line(xy_bear, xy_water)[,1],
   #lon=geosphere::dist2Line(xy_bear, xy_water)[,2],
   #lat=geosphere::dist2Line(xy_bear, xy_water)[,3])
@@ -216,47 +216,11 @@ head(mdist4_df)
 
 
 
-###
-
-mdist5 <- list()
-mdist6 <- list()
-
-for(i in 1:nrow(used_avail_subset)){
-  matching_raster <- water_coordinates[which(names(water_coordinates) == used_avail_subset[i,"date_char"])]
-  water_lat <- coordinates(matching_raster)[,3]
-  water_long <- coordinates(matching_raster)[,2]
-  xy_water <- SpatialPointsDataFrame(
-    matrix(c(water_long, water_lat), ncol=2), data.frame(ID=seq(1:length(water_long))),
-    # proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
-    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # gDistance needs projected coordinates
-  bear_lat <- coordinates(used_avail_subset_spatial)[i,2]
-  bear_long <- coordinates(used_avail_subset_spatial)[i,1]
-  xy_bear <- SpatialPointsDataFrame(
-    matrix(c(bear_long, bear_lat), ncol=2), data.frame(ID=seq(1:length(bear_long))),
-    #proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
-    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # use for gDistance
-  mdist5[[i]] <- as.data.frame(gDistance(xy_water, xy_bear, byid=TRUE))
-  mdist6[[i]] <- unlist(mdist5[which(mdist5==min(mdist5[[i]]))]) # this line gives me this error: "'list' object cannot be coerced to type 'double'"
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
 ###
 
-
-
-
-# test plot 
+# test plot (original - dist2Line)
 
 proj4string(raster_list$'19940404')
 proj4string(water_coordinates$'19940404')
@@ -276,13 +240,6 @@ plot(raster_19940404_latlon, col=(viridis(5)), zlim=c(0, 1))
 plot(water_19940404, pch=20, col=rgb(1, 0, 0, 0.2), add=TRUE)
 points(bear19940404_spatial, col="pink")
 plot(bears_distwater_spatial, col="black", pch=20, add=TRUE)
-
-
-
-
-
-
-
 
 
 # figure out what is going on
@@ -315,7 +272,193 @@ points(difference$lon, difference$lat, col="pink") # IT DOESN'T EVEN PLOT THE WR
 
 
 
-# 4. Loop with a subset that works ----------
+
+
+# 4. Loop #2 with problem subset (using: gDistance again but adding min)  ----------
+
+
+used_avail=subset(used_avail, select=-c(DIST_WATER))
+used_avail_subset <- used_avail %>% filter(date_char==19940404)
+unique(used_avail_subset$date_char)
+used_avail_subset$date_char <- as.numeric(used_avail_subset$date_char)
+used_avail_subset$LAT <- as.numeric(as.character(used_avail_subset$LAT))
+used_avail_subset$LONG <- as.numeric(as.character(used_avail_subset$LONG))
+
+used_avail_subset_spatial <- used_avail_subset
+coordinates(used_avail_subset_spatial) <- ~LONG + LAT
+proj4string(used_avail_subset_spatial) <- CRS("+proj=longlat +datum=WGS84")
+head(used_avail_subset)
+head(used_avail_subset_spatial)
+str(used_avail_subset_spatial)
+
+###
+
+mdist5 <- list()
+mdist6 <- list()
+
+for(i in 1:nrow(used_avail_subset)){
+  matching_raster <- water_coordinates[which(names(water_coordinates) == used_avail_subset[i,"date_char"])]
+  water_lat <- coordinates(matching_raster)[,3]
+  water_long <- coordinates(matching_raster)[,2]
+  xy_water <- SpatialPointsDataFrame(
+    matrix(c(water_long, water_lat), ncol=2), data.frame(ID=seq(1:length(water_long))),
+    # proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # gDistance needs projected coordinates
+  bear_lat <- coordinates(used_avail_subset_spatial)[i,2]
+  bear_long <- coordinates(used_avail_subset_spatial)[i,1]
+  xy_bear <- SpatialPointsDataFrame(
+    matrix(c(bear_long, bear_lat), ncol=2), data.frame(ID=seq(1:length(bear_long))),
+    #proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # use for gDistance
+  mdist5[[i]] <- as.data.frame(gDistance(xy_water, xy_bear, byid=TRUE))
+  mdist6[[i]] <- unlist(mdist5[which(mdist5==min(mdist5[[i]]))]) # this line gives me this error: "'list' object cannot be coerced to type 'double'"
+}
+
+
+
+
+
+# 5. Loop #3 with problem subset (using: st_nearest_point)  ----------
+
+
+used_avail=subset(used_avail, select=-c(DIST_WATER))
+used_avail_subset <- used_avail %>% filter(date_char==19940404)
+unique(used_avail_subset$date_char)
+used_avail_subset$date_char <- as.numeric(used_avail_subset$date_char)
+used_avail_subset$LAT <- as.numeric(as.character(used_avail_subset$LAT))
+used_avail_subset$LONG <- as.numeric(as.character(used_avail_subset$LONG))
+
+used_avail_subset_spatial <- used_avail_subset
+coordinates(used_avail_subset_spatial) <- ~LONG + LAT
+proj4string(used_avail_subset_spatial) <- CRS("+proj=longlat +datum=WGS84")
+head(used_avail_subset)
+head(used_avail_subset_spatial)
+str(used_avail_subset_spatial)
+
+###
+
+mdist7 <- list()
+
+for(i in 1:nrow(used_avail_subset)){
+  matching_raster <- water_coordinates[which(names(water_coordinates) == used_avail_subset[i,"date_char"])]
+  water_lat <- coordinates(matching_raster)[,3]
+  water_long <- coordinates(matching_raster)[,2]
+  xy_water <- SpatialPointsDataFrame(
+    matrix(c(water_long, water_lat), ncol=2), data.frame(ID=seq(1:length(water_long))),
+    # proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # gDistance needs projected coordinates
+  bear_lat <- coordinates(used_avail_subset_spatial)[i,2]
+  bear_long <- coordinates(used_avail_subset_spatial)[i,1]
+  xy_bear <- SpatialPointsDataFrame(
+    matrix(c(bear_long, bear_lat), ncol=2), data.frame(ID=seq(1:length(bear_long))),
+    #proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # use for gDistance
+  mdist7[[i]] <- as.data.frame(st_nearest_points(st_as_sfc(xy_water), st_as_sfc(xy_bear)))
+}
+
+
+class(mdist7)
+mdist7_df <- bind_rows(mdist7) 
+head(mdist7_df)
+str(mdist7_df)
+class(mdist7_df)
+
+
+mdist7_unlist <- unlist(mdist7)
+mdist7_unlist2 <- matrix(mdist7_unlist, ncol=4, byrow=TRUE)
+mdist7_unlist3 <- head(as.data.frame(mdist7_unlist2))
+head(mdist7_unlist3)
+
+# I don't really know what ended up happening there
+
+
+
+###
+
+
+
+# 6. Loop #4 with problem subset (using: nn2)  ----------
+
+
+used_avail=subset(used_avail, select=-c(DIST_WATER))
+used_avail_subset <- used_avail %>% filter(date_char==19940404)
+unique(used_avail_subset$date_char)
+used_avail_subset$date_char <- as.numeric(used_avail_subset$date_char)
+used_avail_subset$LAT <- as.numeric(as.character(used_avail_subset$LAT))
+used_avail_subset$LONG <- as.numeric(as.character(used_avail_subset$LONG))
+
+used_avail_subset_spatial <- used_avail_subset
+coordinates(used_avail_subset_spatial) <- ~LONG + LAT
+proj4string(used_avail_subset_spatial) <- CRS("+proj=longlat +datum=WGS84")
+head(used_avail_subset)
+head(used_avail_subset_spatial)
+str(used_avail_subset_spatial)
+
+###
+
+# https://www.gis-blog.com/nearest-neighbour-search-for-spatial-points-in-r/
+
+mdist8 <- list()
+
+for(i in 1:nrow(used_avail_subset)){
+  matching_raster <- water_coordinates[which(names(water_coordinates) == used_avail_subset[i,"date_char"])]
+  water_lat <- coordinates(matching_raster)[,3]
+  water_long <- coordinates(matching_raster)[,2]
+  xy_water <- SpatialPointsDataFrame(
+    matrix(c(water_long, water_lat), ncol=2), data.frame(ID=seq(1:length(water_long))),
+    # proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # gDistance needs projected coordinates
+  bear_lat <- coordinates(used_avail_subset_spatial)[i,2]
+  bear_long <- coordinates(used_avail_subset_spatial)[i,1]
+  xy_bear <- SpatialPointsDataFrame(
+    matrix(c(bear_long, bear_lat), ncol=2), data.frame(ID=seq(1:length(bear_long))),
+    #proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # use for gDistance
+  mdist8[[i]] <- as.data.frame(nn2(as.data.frame(xy_bear), as.data.frame(xy_water), k=1))
+}
+
+
+class(mdist8)
+mdist8_df <- bind_rows(mdist8) 
+head(mdist8_df)
+unique(mdist8_df$nn.idx) # there's only one ID #, so this seems wrong
+
+
+
+# try the same loop with xy_bear and xy_water switched in mdist8 line
+
+mdist9 <- list()
+
+for(i in 1:nrow(used_avail_subset)){
+  matching_raster <- water_coordinates[which(names(water_coordinates) == used_avail_subset[i,"date_char"])]
+  water_lat <- coordinates(matching_raster)[,3]
+  water_long <- coordinates(matching_raster)[,2]
+  xy_water <- SpatialPointsDataFrame(
+    matrix(c(water_long, water_lat), ncol=2), data.frame(ID=seq(1:length(water_long))),
+    # proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # gDistance needs projected coordinates
+  bear_lat <- coordinates(used_avail_subset_spatial)[i,2]
+  bear_long <- coordinates(used_avail_subset_spatial)[i,1]
+  xy_bear <- SpatialPointsDataFrame(
+    matrix(c(bear_long, bear_lat), ncol=2), data.frame(ID=seq(1:length(bear_long))),
+    #proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")) # use for dist2Line
+    proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")) # use for gDistance
+  mdist9[[i]] <- as.data.frame(nn2(as.data.frame(xy_water), as.data.frame(xy_bear), k=1))
+}
+
+
+class(mdist9)
+mdist9_df <- bind_rows(mdist9) 
+head(mdist9_df)
+unique(mdist9_df$nn.idx) # now there's only 2 ... not sure what's happening
+
+
+
+
+###
+
+
+# 5. Loop with a subset that works (using dist2Line) ----------
 
 used_avail_subset2 <- used_avail %>% filter(date_char==20010311)
 unique(used_avail_subset2$date_char)

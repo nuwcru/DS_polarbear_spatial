4b3330a1272ff2036f13a6613b85753df74109d2
emhedlin <hedlin@ualberta.ca>
2021-05-19 10:52:25 -0600

    added to the clean RSF script to test breakup model

--- scripts/08_RSFs_clean.R
+++ scripts/08_RSFs_clean.R
@@ -20,16 +20,17 @@ used_avail <- read_csv("data/Oct2020work/FINAL DATASET/used_avail_bath_ice_distl
          USED_AVAIL = as.numeric(case_when(
            USED_AVAIL == "available" ~ "0",
            USED_AVAIL == "used" ~ "1"))) %>%
-  dplyr::select(-c(X.1, WATER_ID, WATER_LONG, WATER_LAT, ROWID, ICE_LAND, ZONE, EASTING, NORTHING)) %>% # note, you might have to remove X.1
+  dplyr::select(-c(X1, WATER_ID, WATER_LONG, WATER_LAT, ROWID, ICE_LAND, ZONE, EASTING, NORTHING)) %>% # note, you might have to remove X.1
    # Scale covariates
   mutate(BATH_SCALED = scale_var(BATH),
          DIST_SCALED = scale_var(DIST_LAND),
          CONC_SCALED = scale_var(CONC),
-         DIST_WATER_SCALED = scale_var(DIST_WATER_M)) %>%
+         DIST_WATER_SCALED = scale_var(DIST_WATER_M)) 
   
 
     
 hist(used_avail$DIST_WATER_SCALED)
+
 # separate used from available for visualzing data
 used <- used_avail %>% filter(USED_AVAIL=="used") # 1463
 avail <- used_avail %>% filter(USED_AVAIL=="available") # 73,150
@@ -56,8 +57,8 @@ fewer_20 <- used_avail %>%
  
  # Add weights column to be used in the models
  used_avail$W <- ifelse(used_avail$USED_AVAIL == 1, 1, 1000)
- 
 
+ 
 ## * Null -------------------------------------------------------------------
 
  
@@ -113,4 +114,55 @@ summary(null)
                        weights = W)
  
  summary(model4_free_var)
+ 
+ 
+
+# Seasonal Models ---------------------------------------------------------
+ 
+ 
+ # We've cleaned the data (removed bears with < 21 fixes), so to do model specific to a season, we just subset within the model
+ 
+ 
+
+
+ used_avail %>%
+   filter(SEASON == "break") %>%
+   ggplot() +
+   geom_histogram(aes(DIST_WATER_SCALED)) +
+   facet_grid(~ID)+
+   nuwcru::theme_nuwcru() + nuwcru::facet_nuwcru()
+ 
+ # ignore the first attempt, we get errors and  NAs for AIC, BIC, LogLik, and deviance
+ model4_tmp_break <- glmmTMB(USED_AVAIL~DIST_WATER_SCALED+(1|ID)+(0+DIST_WATER_SCALED|ID), 
+                             family=binomial(), 
+                             data=subset(used_avail, SEASON = "break"), 
+                             doFit=F, 
+                             weights=W)
+ model4_tmp_break$parameters$theta[1] = log(1e3)
+ model4_tmp_break$mapArg = list(theta = factor(c(NA, 1:1)))
+ model4_break <- glmmTMB:::fitTMB(model4_tmp_break) 
+ summary(model4_break)
+ 
+ # Instead, don't fix intercept variance, but estimate it from the data
+ # Error here too: "Model convergence problem; non-positive-definite Hessian matrix"
+ model4_break_free_var <- glmmTMB(USED_AVAIL~DIST_WATER_SCALED+(1|ID)+(0+DIST_WATER_SCALED|ID), 
+                                  family=binomial(), 
+                                  data=subset(used_avail, SEASON = "break"), 
+                                  doFit=TRUE, 
+                                  weights=W)
+ summary(model4_break_free_var)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  

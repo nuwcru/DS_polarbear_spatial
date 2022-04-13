02e930c36f16934a06b2012f29c4194cc5266dfe
Larissa Thelin <larissathelin@Larissas-MacBook-Pro.local>
2022-04-13 12:02:33 -0700

    fixed seasonal UDs

--- scripts/10_SeasonalUDs.R
+++ scripts/10_SeasonalUDs.R
@@ -61,24 +61,40 @@ used_breakup <- used_avail_RSF_breakup_FINAL %>% filter(USED_AVAIL==1)
 head(used_freezeup)
 colnames(used_freezeup)
 
-# create pilot first
+# create pilot and run kde
 freezeup_pilot <- Hpi(used_freezeup[, 11:12], pilot="amse", binned=T) 
-
-# plot
 freezeup_kernel <- kde(used_freezeup[, 11:12], H=freezeup_pilot)
+
+# get contours and plot
 freezeup_contour95 <- contourLevels(freezeup_kernel, cont=95) # 95%
 freezeup_contour50 <- contourLevels(freezeup_kernel, cont=50) # 50%
 freezeup_line95 = contourLines(x = freezeup_kernel$eval.points[[1]], y = freezeup_kernel$eval.points[[2]], z = freezeup_kernel$estimate, level = freezeup_contour95)
 freezeup_line50 = contourLines(x = freezeup_kernel$eval.points[[1]], y = freezeup_kernel$eval.points[[2]], z = freezeup_kernel$estimate, level = freezeup_contour50)
-#freezeup_sldf95 = ContourLines2SLDF(freezeup_line95, proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=75"))
-#freezeup_sldf50 = ContourLines2SLDF(freezeup_line50, proj4string=CRS("+proj=stere +lat_0=90 +lat_ts=75"))
 freezeup_sldf95 = ContourLines2SLDF(freezeup_line95, proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 freezeup_sldf50 = ContourLines2SLDF(freezeup_line50, proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 plot(freezeup_sldf95)
 plot(freezeup_sldf50, add=TRUE, col="blue")
 
-writeOGR(freezeup_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_freezeup50.shp", layer="freezeup_sldf50", driver="ESRI Shapefile")
-writeOGR(freezeup_sldf95, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_freezeup95.shp", layer="freezeup_sldf95", driver="ESRI Shapefile")
+# make into polygon 
+freezeup_polyset95 = SpatialLines2PolySet(freezeup_sldf95) 
+freezeup_polygon95 = PolySet2SpatialPolygons(freezeup_polyset95) 
+freezeup_polyset50 = SpatialLines2PolySet(freezeup_sldf50) 
+freezeup_polygon50 = PolySet2SpatialPolygons(freezeup_polyset50) 
+# compare to test
+plot(freezeup_sldf95)
+plot(freezeup_polygon95, add=TRUE, col="lightblue") # looks good!
+
+# export spatial polygon
+freezeup_polygon95_final <- SpatialPolygonsDataFrame(freezeup_polygon95, data=as.data.frame("freezeup_polygon95", proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+freezeup_polygon50_final <- SpatialPolygonsDataFrame(freezeup_polygon50, data=as.data.frame("freezeup_polygon50", proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+
+writeOGR(freezeup_polygon95_final, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_freezeup95_final.shp", layer="freezeup_polygon95_final", driver="ESRI Shapefile")
+writeOGR(freezeup_polygon50_final, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_freezeup50_final.shp", layer="freezeup_polygon50_final", driver="ESRI Shapefile", overwrite=TRUE)
+
+
+
+#writeOGR(freezeup_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_freezeup50.shp", layer="freezeup_sldf50", driver="ESRI Shapefile")
+#writeOGR(freezeup_sldf95, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_freezeup95.shp", layer="freezeup_sldf95", driver="ESRI Shapefile")
 
 
 ###
@@ -98,11 +114,11 @@ colnames(used_breakup)
 head(used_freezeup)
 colnames(used_freezeup)
 
-# create pilot first
+# create pilot and kde
 breakup_pilot <- Hpi(used_breakup[, 11:12], pilot="amse", binned=T) 
-
-# plot
 breakup_kernel <- kde(used_breakup[, 11:12], H=breakup_pilot)
+
+# create contours and plot
 breakup_contour95 <- contourLevels(breakup_kernel, cont=95) # 95%
 breakup_contour50 <- contourLevels(breakup_kernel, cont=50) # 50%
 breakup_line95 = contourLines(x = breakup_kernel$eval.points[[1]], y = breakup_kernel$eval.points[[2]], z = breakup_kernel$estimate, level = breakup_contour95)
@@ -112,8 +128,27 @@ breakup_sldf50 = ContourLines2SLDF(breakup_line50, proj4string=CRS("+proj=longla
 plot(breakup_sldf95)
 plot(breakup_sldf50, add=TRUE, col="blue")
 
-writeOGR(breakup_sldf95, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_breakup95.shp", layer="breakup_sldf95", driver="ESRI Shapefile")
-writeOGR(breakup_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_breakeup50.shp", layer="breakup_sldf50", driver="ESRI Shapefile")
+# make into polygon
+breakup_polyset95 = SpatialLines2PolySet(breakup_sldf95) 
+breakup_polygon95 = PolySet2SpatialPolygons(breakup_polyset95) 
+breakup_polyset50 = SpatialLines2PolySet(breakup_sldf50) 
+breakup_polygon50 = PolySet2SpatialPolygons(breakup_polyset50) 
+# compare to test
+plot(breakup_sldf95)
+plot(breakup_polygon95, add=TRUE, col="lightblue") # looks good!
+
+# export spatial polygon
+breakup_polygon95_final <- SpatialPolygonsDataFrame(breakup_polygon95, data=as.data.frame("breakup_polygon95", proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+breakup_polygon50_final <- SpatialPolygonsDataFrame(breakup_polygon50, data=as.data.frame("breakup_polygon50", proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+
+writeOGR(breakup_polygon95_final, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_breakup95_final.shp", layer="breakup_polygon95_final", driver="ESRI Shapefile")
+writeOGR(breakup_polygon50_final, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_breakup50_final.shp", layer="breakup_polygon50_final", driver="ESRI Shapefile", overwrite=TRUE)
+
+
+###
+
+#writeOGR(breakup_sldf95, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_breakup95.shp", layer="breakup_sldf95", driver="ESRI Shapefile")
+#writeOGR(breakup_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_breakeup50.shp", layer="breakup_sldf50", driver="ESRI Shapefile")
 
 
 
@@ -125,11 +160,11 @@ writeOGR(breakup_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Ma
 head(used_winter)
 colnames(used_winter)
 
-# create pilot first
+# create pilot and kde
 winter_pilot <- Hpi(used_winter[, 11:12], pilot="amse", binned=T) 
-
-# plot
 winter_kernel <- kde(used_winter[, 11:12], H=winter_pilot)
+
+# create contours
 winter_contour95 <- contourLevels(winter_kernel, cont=95) # 95%
 winter_contour50 <- contourLevels(winter_kernel, cont=50) # 50%
 winter_line95 = contourLines(x = winter_kernel$eval.points[[1]], y = winter_kernel$eval.points[[2]], z = winter_kernel$estimate, level = winter_contour95)
@@ -139,7 +174,29 @@ winter_sldf50 = ContourLines2SLDF(winter_line50, proj4string=CRS("+proj=longlat 
 plot(winter_sldf95)
 plot(winter_sldf50, add=TRUE, col="blue")
 
-writeOGR(winter_sldf95, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_winter95.shp", layer="winter_sldf95", driver="ESRI Shapefile")
-writeOGR(winter_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_winter50.shp", layer="winter_sldf50", driver="ESRI Shapefile")
+# make into polygon
+winter_polyset95 = SpatialLines2PolySet(winter_sldf95) 
+winter_polygon95 = PolySet2SpatialPolygons(winter_polyset95) 
+winter_polyset50 = SpatialLines2PolySet(winter_sldf50) 
+winter_polygon50 = PolySet2SpatialPolygons(winter_polyset50) 
+# compare to test
+plot(winter_sldf95)
+plot(winter_polygon95, add=TRUE, col="lightblue") # looks good!
+
+# export spatial polygon
+winter_polygon95_final <- SpatialPolygonsDataFrame(winter_polygon95, data=as.data.frame("winter_polygon95", proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+winter_polygon50_final <- SpatialPolygonsDataFrame(winter_polygon50, data=as.data.frame("winter_polygon50", proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+
+writeOGR(winter_polygon95_final, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_winter95_final.shp", layer="winter_polygon95_final", driver="ESRI Shapefile")
+writeOGR(winter_polygon50_final, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_winter50_final.shp", layer="winter_polygon50_final", driver="ESRI Shapefile", overwrite=TRUE)
+
+
+
+
+
+###
+
+#writeOGR(winter_sldf95, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_winter95.shp", layer="winter_sldf95", driver="ESRI Shapefile")
+#writeOGR(winter_sldf50, dsn="/Volumes/Larissa G-drive/UAlberta MSc/Thesis/2. Mapping/UDs_2022/bears_winter50.shp", layer="winter_sldf50", driver="ESRI Shapefile")
 
 
